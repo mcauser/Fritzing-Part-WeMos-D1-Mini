57f0d7a4a5f76651f9cb3eef3abde62fb767cc17
Mike Causer <mcauser@gmail.com>
2016-02-10 18:28:38 +1100

    Initial

---
+++ README.md
@@ -0,0 +1,20 @@
+Fritzing Part: WeMos D1 Mini
+===================================
+
+Fritzing part for the WeMos D1 Mini
+
+<https://wemos.cc>
+
+<http://fritzing.org>
+
+#### Breadboard:
+
+![Fritzing Breadboard](https://raw.github.com/mcauser/Fritzing-Part-WeMos-D1-Mini/master/demo/breadboard.png)
+
+#### Schematic:
+
+![Fritzing Schematic](https://raw.github.com/mcauser/Fritzing-Part-WeMos-D1-Mini/master/demo/schematic.png)
+
+#### PCB:
+
+![Fritzing PCB](https://raw.github.com/mcauser/Fritzing-Part-WeMos-D1-Mini/master/demo/pcb.png)
---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+rm -rf dist/WeMos-D1-mini-female-headers-above.fzpz
+zip -j dist/WeMos-D1-mini-female-headers-above.fzpz src/part.WeMos-D1-mini-female-headers-above.fzp
+zip -j dist/WeMos-D1-mini-female-headers-above.fzpz src/svg.breadboard.WeMos-D1-mini-female-headers-above_breadboard.svg
+zip -j dist/WeMos-D1-mini-female-headers-above.fzpz src/svg.icon.WeMos-D1-mini-female-headers-above_icon.svg
+zip -j dist/WeMos-D1-mini-female-headers-above.fzpz src/svg.pcb.WeMos-D1-mini_pcb.svg
+zip -j dist/WeMos-D1-mini-female-headers-above.fzpz src/svg.schematic.WeMos-D1-mini_schematic.svg
+
+rm -rf dist/WeMos-D1-mini-male-headers-above.fzpz
+zip -j dist/WeMos-D1-mini-male-headers-above.fzpz src/part.WeMos-D1-mini-male-headers-above.fzp
+zip -j dist/WeMos-D1-mini-male-headers-above.fzpz src/svg.breadboard.WeMos-D1-mini-male-headers-above_breadboard.svg
+zip -j dist/WeMos-D1-mini-male-headers-above.fzpz src/svg.icon.WeMos-D1-mini-male-headers-above_icon.svg
+zip -j dist/WeMos-D1-mini-male-headers-above.fzpz src/svg.pcb.WeMos-D1-mini_pcb.svg
+zip -j dist/WeMos-D1-mini-male-headers-above.fzpz src/svg.schematic.WeMos-D1-mini_schematic.svg
+
+rm -rf dist/WeMos-D1-mini-male-headers-below.fzpz
+zip -j dist/WeMos-D1-mini-male-headers-below.fzpz src/part.WeMos-D1-mini-male-headers-below.fzp
+zip -j dist/WeMos-D1-mini-male-headers-below.fzpz src/svg.breadboard.WeMos-D1-mini-male-headers-below_breadboard.svg
+zip -j dist/WeMos-D1-mini-male-headers-below.fzpz src/svg.icon.WeMos-D1-mini-male-headers-below_icon.svg
+zip -j dist/WeMos-D1-mini-male-headers-below.fzpz src/svg.pcb.WeMos-D1-mini_pcb.svg
+zip -j dist/WeMos-D1-mini-male-headers-below.fzpz src/svg.schematic.WeMos-D1-mini_schematic.svg
+
+rm -rf dist/WeMos-D1-mini-no-headers.fzpz
+zip -j dist/WeMos-D1-mini-no-headers.fzpz src/part.WeMos-D1-mini-no-headers.fzp
+zip -j dist/WeMos-D1-mini-no-headers.fzpz src/svg.breadboard.WeMos-D1-mini-no-headers_breadboard.svg
+zip -j dist/WeMos-D1-mini-no-headers.fzpz src/svg.icon.WeMos-D1-mini-no-headers_icon.svg
+zip -j dist/WeMos-D1-mini-no-headers.fzpz src/svg.pcb.WeMos-D1-mini_pcb.svg
+zip -j dist/WeMos-D1-mini-no-headers.fzpz src/svg.schematic.WeMos-D1-mini_schematic.svg
